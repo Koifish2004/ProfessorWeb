bd7de0b61b7cb60142a62113adbda48d6ce9094d
Kaif Khan <calmdownkaif@gmail.com>
2025-11-09 04:39:37 +0530

    Minor changes lol

--- grademyprofAuth/main.go
+++ grademyprofAuth/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 
 func main(){
 	fmt.Println("Hello World")
+	err := godotenv.Load()
+	if err != nil {
+    	log.Fatal("Error loading .env file")
+}
 }
--- grademyprofUI/src/firebase/config.ts
+++ grademyprofUI/src/firebase/config.ts
@@ -1,15 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCbKmbx3MQ8CXU9620X6oLEGxDDyLwGgU8",
-  authDomain: "grademyprof-d35a4.firebaseapp.com",
-  projectId: "grademyprof-d35a4",
-  storageBucket: "grademyprof-d35a4.firebasestorage.app",
-  messagingSenderId: "511570494587",
-  appId: "1:511570494587:web:d8d41b1b2b795d8368cede",
-  measurementId: "G-3M5ZCGW90N",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
